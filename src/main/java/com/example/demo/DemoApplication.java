1c2f92517a8f36140e7d60a1491612741069a866
noah <qkrwldn0818@naver.com>
2024-03-16 18:59:36 +0900

    로그추가

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -6,8 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+	public static void main(String[] test) {
+		System.out.println("시스템 활성화 인입");
+		SpringApplication.run(DemoApplication.class, test);
+		System.out.println("시스템 활성화 완료");
 	}
 
 }
